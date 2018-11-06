742858808f85bafcda7a4cb90c066b29c774aa15
marinavraric <marina.vrataric@gmail.com>
2018-11-06 22:54:58 +0100

    prvi commit

---
+++ Program (1).cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RUAP_Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Pozdrav iz aplikacije od Marine Vratarić");
+        }
+    }
+}
